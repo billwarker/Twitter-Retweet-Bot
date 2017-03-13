9e56049a43b02e2ebdaeb26383df4c85402af7d7
Will Barker <willbarker1995@gmail.com>
2017-03-13 02:56:17 -0400

    Add files via upload

--- SETTINGS.py
+++ SETTINGS.py
@@ -1,10 +1,10 @@
 # Settings
 
-# Twitter Authetication
-CONSUMER_KEY = "l3j8jY1YXtCoAue2R4bo1EYxK"
-CONSUMER_SECRET = "VQK8P89nZ7EzQSGPZyFNgbQ4vS9dJXHnQIHVHyWizii0PAPP8s"
-ACCESS_TOKEN_KEY = "826610881588899840-zsEegF9NIu7pcBlbWFffvpsFKKNKq3f"
-ACCESS_TOKEN_SECRET = "NwdU1WiUPnHI7Ex6VnwETiaFauFtQp7ydLbmbayfWfDEF"
+# Twitter Authetication (add your own keys/tokens!)
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN_KEY = ""
+ACCESS_TOKEN_SECRET = ""
 
 # Search parameters
 KEYWORDS = ['Artificial Intelligence', 'Machine Learning', 'Big Data', 'Tensorflow']
